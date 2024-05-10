fd2eb47ccbedcceec4acc64d871a4cc67589a426
thomtax <thomastax03@gmail.com>
2024-05-10 15:30:40 +0200

    modifiche Deductions, non funziona il cambio scheda

--- src/main/java/com/example/projectapp/Deductions.java
+++ src/main/java/com/example/projectapp/Deductions.java
@@ -1,4 +1,12 @@
 package com.example.projectapp;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+
 public class Deductions {
+    @FXML
+    private CheckBox idINPS;
+    public void onINPS (){
+
+    }
 }
